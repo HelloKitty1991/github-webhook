a570270201ea7b3a16b118fb6c6c178ed1fca6c2
penganhua <penganhua@qmtv.com>
2017-12-30 16:17:05 +0800

    update the deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 cd /alidata/root
 git reset origin/master --hard
 mvn package -Dmaven.test.skip=true
+echo 'copy the ROOT.war to server';
 cd /alidata/root/personal/target
 \cp -f ROOT.war /alidata/server/tomcat7/webapps/
