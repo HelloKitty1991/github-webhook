b6c039361fa13bb34920a78d614a7f185651f9fb
penganhua <penganhua@qmtv.com>
2017-12-31 14:53:07 +0800

    add one more path config

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
-var http = require('http')
-var createHandler = require('github-webhook-handler')
-var handler = createHandler({
-    path: '/apollo-webhook', secret: 'IAv38Q9uPyZTM2Pr',
-    path:'/personal-static-webhook',secret:'Fg3ITv6aUxCYMGsM'
+const http = require('http')
+const createHandler = require('github-webhook-handler')
+const handlerOptions = [{
+    path: '/apollo-webhook', secret: 'IAv38Q9uPyZTM2Pr'
+}, {
+    path: '/personal-static-webhook', secret: 'Fg3ITv6aUxCYMGsM'
+}]
+const handlerMap = {};
+handlerOptions.forEach(option =>{
+    handlerMap[option.path] = createHandler(option);
+
+    handlerMap[option.path].on('error', function (err) {
+        console.error('Error:', err.message)
+    })
+
+    handlerMap[option.path].on('push', function (event) {
+        console.log('Received a push event for %s to %s',
+            event.payload.repository.name,
+            event.payload.ref);
+        run_cmd('sh', [`./${event.payload.repository.name}.deploy.sh`]);
+    })
 });
 
 function run_cmd(cmd, args) {
-    var spawn = require('child_process').spawn;
-    var child = spawn(cmd, args);
+    const spawn = require('child_process').spawn;
+    const child = spawn(cmd, args);
 
-    child.stdout.on('data', function(buffer) { console.log(buffer.toString());});
+    child.stdout.on('data', function (buffer) {
+        console.log(buffer.toString());
+    });
 }
 
 http.createServer(function (req, res) {
-    handler(req, res, function (err) {
+    handlerMap[req.url.split('?').shift()](req, res, function () {
         res.statusCode = 404
         res.end('no such location')
     })
-}).listen(7777,function(){
-   console.log('listening in 7777 ....')
+}).listen(7777, function () {
+    console.log('listening in 7777 ....')
 });
-
-handler.on('error', function (err) {
-    console.error('Error:', err.message)
-})
-
-handler.on('push', function (event) {
-    console.log('Received a push event for %s to %s',
-        event.payload.repository.name,
-        event.payload.ref);
-    run_cmd('sh', [`./${event.payload.repository.name}.deploy.sh`]);
-})
