5d419de11be5c5017be7b68adcad3349e265d6ad
penganhua <penganhua@qmtv.com>
2017-12-31 13:58:16 +0800

    add personal-static webhook

--- app.js
+++ app.js
@@ -30,5 +30,5 @@ handler.on('push', function (event) {
     console.log('Received a push event for %s to %s',
         event.payload.repository.name,
         event.payload.ref);
-    run_cmd('sh', ['./deploy.sh']);
+    // run_cmd('sh', ['./deploy.sh']);
 })
