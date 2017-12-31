ebc2d2394dba95d7d583e4cb586becda782166a5
penganhua <penganhua@qmtv.com>
2017-12-31 13:51:46 +0800

    update the deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,23 @@
 cd /alidata/root
+
+echo '==================>pull the code from git repo<==================='
 git pull
+
+echo '==================>build the project<==================='
 mvn package -Dmaven.test.skip=true
-echo 'copy the ROOT.war to server......';
+
+echo '==================>shutdown the tomcat server<==================='
+cd /alidata/server/tomcat7/bin
+sh shutdown.sh
+
+echo '==================>copy the ROOT.war to server<===================';
 cd /alidata/root/personal/target
 \cp -f ROOT.war /alidata/server/tomcat7/webapps/
-echo 'delete the ROOT directory......'
+
+echo '==================>delete the ROOT directory<==================='
+cd /alidata/server/tomcat7/webapps
 rm -rf ROOT
-echo 'restart the server......'
-#cd /alidata/server/tomcat7/bin
-#sh shutdown.sh
-#sh startup.sh
+
+echo '==================>restart the server<==================='
+cd /alidata/server/tomcat7/bin
+sh startup.sh
