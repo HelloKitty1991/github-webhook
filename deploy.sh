0d895e20cd895ba73d2efabfb4f19f20efc4fdc8
penganhua <penganhua@qmtv.com>
2017-12-31 13:08:31 +0800

    update the deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ mvn package -Dmaven.test.skip=true
 echo 'copy the ROOT.war to server';
 cd /alidata/root/personal/target
 \cp -f ROOT.war /alidata/server/tomcat7/webapps/
+rm -rf ROOT
 echo 'restart the server'
-cd /alidata/server/tomcat7/bin
-sh shutdown.sh
-sh startup.sh
+#cd /alidata/server/tomcat7/bin
+#sh shutdown.sh
+#sh startup.sh
